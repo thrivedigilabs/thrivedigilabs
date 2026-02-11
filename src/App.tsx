8cfb0a0149dbbb96b3e562bc19c2fb0ae11930ef
thrivedigilabs <thrivedigilabs@gmail.com>
2026-02-12 02:55:32 +0530

    Remove basename from BrowserRouter
    Removed basename from BrowserRouter in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/thrivedigilabs">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/portfolio" element={<Portfolio />} />
